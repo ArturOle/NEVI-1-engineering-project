0bedc4afd9c8d788c66bffb8cbbdb63e210883f6
ArturOle <artur.oleksinski99@gmail.com>
2022-09-27 09:51:48 +0200

    prepaaration for image takeover for image analysis

--- API/main.py
+++ API/main.py
@@ -1,5 +1,6 @@
 # Built-in
 import os
+import logging
 
 # FastAPI
 from fastapi import FastAPI, UploadFile
@@ -10,14 +11,19 @@ from fastapi.templating import Jinja2Templates
 import uuid
 
 # Firebase
-from firebase_admin import credentials, initialize_app
+from firebase_admin import credentials, db, initialize_app
 from google.cloud import storage
 
+logging.basicConfig(
+    format='%(levelname)s:%(message)s',
+    level=logging.INFO
+)
+log = logging.getLogger(__name__)
 
-class MetaData(BaseModel):
-    data_1: str = None
-    size_x: float = None
-    size_y: float = None
+
+# FastAPI initialization
+app = FastAPI(debug=False)
+templates = Jinja2Templates(directory="templates")
 
 
 # Firebase initialization
@@ -27,10 +33,28 @@ fire = initialize_app(cred)
 client = storage.Client()
 bucket = client.get_bucket('nevi-59237.appspot.com')
 client.list_buckets()
+ref = db.reference(
+    path="/posts",
+    url="https://nevi-59237-default-rtdb.europe-west1.firebasedatabase.app"
+)
 
-# FastAPI initialization
-app = FastAPI(debug=False)
-templates = Jinja2Templates(directory="templates")
+
+class MetaData(BaseModel):
+    data_1: str = None
+    size_x: float = None
+    size_y: float = None
+
+
+def listner(event):
+    log.info(
+        'Data: event_type={} path={} other={}'.format(
+            event.event_type,
+            event.path,
+            event.data
+        )
+    )
+    if event.event_type == "patch":
+        print(event.event_data)
 
 
 def save_file(filename, data):
@@ -74,19 +98,35 @@ async def firebase_post(file: Optional[UploadFile] = None):
 
 @app.get("/img_in_db/", response_class=HTMLResponse)
 async def show_files():
+    files = ref.get()
+    images = {}
+
+    for hash_key, case_list in files.items():
+        for dictionary in case_list:
+            images[dictionary] = files[hash_key][dictionary]["image_url"]
+
+    data = [
+        f"<h2>{hash_key}</h2>\n<img src={image}>"
+        for hash_key, image in images.items()
+    ]
+
     return """
         <html>
         <head>
             <title>Item Details</title>
         </head>
         <body>
-            <h1>Files: {files}</h1>
+            <h1>Files:</h1>
+            {}
         </body>
         </html>
-    """.format(files="\n".join(["a", "b"]))
+    """.format('\n'.join(data))
 
 
 @app.post("/images/")
 async def create_upload_file(file: Optional[UploadFile] = None):
     # db.append(file.filename)
     return {"filename": file.filename}
+
+
+ref.listen(listner)
