13b966f2c3d6388c200dc42501fcf808ce01e36b
ArturOle <artur.oleksinski99@gmail.com>
2022-10-13 20:10:48 +0200

    Predictor

--- API/predictor.py
+++ API/predictor.py
@@ -1,9 +1,8 @@
-from PIL import Image
-
-import numpy as np
 from skimage import transform
-
+from PIL import Image
+from keras.applications.efficientnet_v2 import preprocess_input
 from keras.models import load_model
+import numpy as np
 
 
 class Predictor:
@@ -11,10 +10,10 @@ class Predictor:
         self.model = load_model(model_path)
 
     def predict(self, image):
-        image = Image.open(image)
-        np_image = np.array(image).astype('float32')/255
+        np_image = np.array(image).astype(float)
         np_image = transform.resize(np_image, (256, 256, 3))
         np_image = np.expand_dims(np_image, axis=0)
+        np_image = preprocess_input(np_image)
         result = self.model.predict(np_image)[0]
         print(result)
         return self.map_results(result)
@@ -29,11 +28,12 @@ class Predictor:
             5: "Melanocytic nevi",
             6: "Vascular lesions"
         }
-        return map_results[np.array([np.argmax(x) for x in results])]
+        return map_results[np.argmax(results)]
 
 
 if __name__ == "__main__":
-    img = Image.open(r"D:\Projects\thesis\secret_test_folder\mel\ISIC_0034243.jpg")
+    # with open(r"D:\Projects\thesis\grouped_images_by_severity\benign\ISIC_0024306.jpg", "r") as f:
+    img = Image.open(r"D:\Projects\thesis\grouped_images_by_type\nv\ISIC_0034098.jpg")
     pred = Predictor(r"D:\Projects\thesis\model\NEVI_0.2.0.h5")
     pred = pred.predict(img)
     print(pred)
